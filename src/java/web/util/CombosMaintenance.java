62baaea9c8d7f9048a0016fc391e5413a3d378ca
Daniel <Dqalta@gmail.com>
2019-06-15 11:55:31 -0400

    Project Configuration
    Database connection, configuration files, tomcat register and namespaces creation. Building the Structure of  project.

---
+++ src/java/web/util/CombosMaintenance.java
@@ -0,0 +1,41 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package web.util;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import org.hibernate.Session;
+//import sql.masonryAdmin.maintenance.DtoMaterial;
+//import sql.masonryAdmin.maintenance.MaintenanceSQL;
+//import sql.masonryAdmin.maintenance.DtoMetricsSystem;
+//
+//
+///**
+// *
+// * @author CR104978
+// */
+//public class CombosMaintenance {
+//
+//     public static ArrayList<KeyCombos> getMaterials(Session mdk) {
+//        ArrayList<KeyCombos> combo = new ArrayList<>();
+//        combo.add(new KeyCombos(0, "SELECT ONE CATEGORY"));
+//        List<DtoMaterial> materials = MaintenanceSQL.getMaterials(mdk);
+//        for (DtoMaterial c : materials) {
+//            combo.add(new KeyCombos((c.getId()), c.getDescription()));
+//        }
+//        return combo;
+//    }
+//      public static ArrayList<KeyCombos> getUnits(Session mdk) {
+//        ArrayList<KeyCombos> combo = new ArrayList<>();
+//        combo.add(new KeyCombos(0, "SELECT ONE METRIC SYSTEM"));
+//        List<DtoMetricsSystem> units = MaintenanceSQL.getMetricsSystems(mdk);
+//        for (DtoMetricsSystem c : units) {
+//            combo.add(new KeyCombos((c.getId()), c.getDescription()));
+//        }
+//        return combo;
+//    }
+//
+//}
---
+++ web/home/main.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : main
+    Created on : 15/06/2019, 11:13:00 AM
+    Author     : developer
+--%>
+<%-- insertion of valid struts labels--%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="/struts-tags" prefix="st"%>
+<%@ taglib uri="/struts-jquery-tags" prefix="stj"%>
+<%@ taglib uri="/struts-bootstrap-tags" prefix="stb"%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <st:include value="/generals/css-js-Head.jsp" />
+        <st:include value="/generals/css-js-Bootstrap.jsp"/>
+        <st:include value="/generals/css-js-app.jsp"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
